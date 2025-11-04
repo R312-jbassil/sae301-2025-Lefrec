9641e3388585869932b7684ac64dd798d0ffecaf
Lefrec <blackfox244@gmail.com>
2025-11-05 00:55:31 +0100

    please

--- src/pages/api/login.js
+++ src/pages/api/login.js
@@ -1,19 +1,20 @@
 import PocketBase from "pocketbase";
 
-const pb = new PocketBase("http://127.0.0.1:8071"); // internal server address
+const pb = new PocketBase("http://127.0.0.1:8071"); // internal PB server
 
 export async function POST({ request, cookies }) {
-  const { email, password } = await request.json();
-
   try {
+    const { email, password } = await request.json();
+
+    // authenticate user
     await pb.collection("users").authWithPassword(email, password);
 
-    // save token in HttpOnly cookie
+    // store token in cookie (HttpOnly)
     cookies.set("pb_auth", pb.authStore.token, {
       path: "/",
       httpOnly: true,
-    //   secure: true, // must be HTTPS
       sameSite: "lax",
+      secure: false,
     });
 
     return new Response(JSON.stringify({ success: true }), { status: 200 });
