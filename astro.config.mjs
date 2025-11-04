75ea8044fe3ae7feb0678197a4e03efaf16befbe
Lefrec <blackfox244@gmail.com>
2025-11-04 21:00:11 +0100

    still testing how cookies work

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,13 +7,11 @@ import node from '@astrojs/node';
 
 // https://astro.build/config
 export default defineConfig({
-  output: 'server',
-
   vite: {
     plugins: [tailwindcss()]
   },
-
   adapter: node({
     mode: 'standalone'
-  })
+  }),
+  output: 'server'
 });
