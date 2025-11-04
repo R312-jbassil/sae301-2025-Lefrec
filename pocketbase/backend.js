071f02a1c812218f949743a5e8726579e50738f8
Lefrec <blackfox244@gmail.com>
2025-11-04 11:20:18 +0100

    basic signin and login

--- pocketbase/backend.js
+++ pocketbase/backend.js
@@ -6,6 +6,35 @@ export async function getTeinteVerre() {
         const TeinteVerre = await pb.collection("TeinteVerre").getFullList();
         return TeinteVerre;
     } catch (error) {
-        return error;
+        console.error(error);
+    }
+}
+
+export async function createUser(data) {
+    try {
+        await pb.collection("users").create({
+            Prenom : data.prenom,
+            Nom : data.nom,
+            email : data.email,
+            password : data.password,
+            passwordConfirm : data.passwordConfirm,
+        });
+        return "Ton compte a bien été créé"
+    } catch (error) {
+        console.error(error);
+        return "Ton compte n'a pas pu être créé"
+    }
+}
+
+export async function authUser(email, password) {
+    try {
+        await pb.collection("users").authWithPassword(
+            email,
+            password
+        );
+        location.href = "/";
+        return;
+    } catch (error) {
+        console.error(error);
     }
 }
