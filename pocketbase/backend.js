bd21dd7e294e3ef4ae430d8074994d7f905644a3
Lefrec <blackfox244@gmail.com>
2025-11-04 14:47:27 +0100

    deploy with github actions

--- pocketbase/backend.js
+++ pocketbase/backend.js
@@ -1,5 +1,6 @@
 import Pocketbase from "pocketbase";
-const pb = new Pocketbase('http://127.0.0.1:8090');
+// const pb = new Pocketbase('http://127.0.0.1:8090');
+const pb = new Pocketbase('https://TaVue.paolo-vincent.fr:443');
 
 //_Fonctions d'authentification_________________________________
 
