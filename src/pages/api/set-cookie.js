50c87ed8ba23a4fa44d91220f2b9ec852aa7c707
Lefrec <blackfox244@gmail.com>
2025-11-05 00:24:15 +0100

    minor modification of the API

--- src/pages/api/set-cookie.js
+++ src/pages/api/set-cookie.js
@@ -1,5 +1,12 @@
 export async function POST({ request, cookies }) {
   const { token } = await request.json();
-  cookies.set('pb_auth', token, { path: '/', httpOnly: true, secure: true });
-  return new Response('OK');
+
+  cookies.set("pb_auth", token, {
+    path: "/",        // required
+    httpOnly: true,   // secure storage, invisible to JS
+    sameSite: "lax",  // or "none" if cross-domain
+    // secure: true      // required for HTTPS, disable only for localhost testing
+  });
+
+  return new Response(JSON.stringify({ success: true }), { status: 200 });
 }
